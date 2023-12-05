ce2e24a1689b45baebbd9568fac903b9c5093fe5
bas-ene <enea.basilico@gmail.com>
2023-12-05 17:59:02 +0100

    bohg, tanta roba i guess

--- First_Day/first_part.cpp
+++ First_Day/first_part.cpp
@@ -1,26 +1,25 @@
 #include <fstream>
+#include <iostream>
 #include <string>
 int main() {
-	long long sum = 0;
+  long long sum = 0;
 
-	std::ifstream fin = std::ifstream("input.txt");
-	std::ofstream fout = std::ofstream("output.txt");
-	std::string line;
-	while (!fin.eof())
-	{
-		char fc = 'a';
-		char lc = 'a';
-		std::getline(fin, line);
-		for (int i = 0; i < line.length(); i++)
-		{
-			if (std::isdigit(line[i])) {
-				if (fc == 'a')
-					fc = line[i];
-				lc = line[i];
-			}
-		}
-		sum += std::stoi(std::string({ fc,lc }));
-
-	}
-	fout << sum;
+  std::ifstream fin = std::ifstream("input.txt");
+  std::ofstream fout = std::ofstream("output.txt");
+  std::string line;
+  while (!fin.eof()) {
+    char fc = 'a';
+    char lc = 'a';
+    std::getline(fin, line);
+    for (int i = 0; i < line.length(); i++) {
+      if (std::isdigit(line[i])) {
+        if (fc == 'a')
+          fc = line[i];
+        lc = line[i];
+      }
+    }
+    std::cout << fc << lc << '\n';
+    sum += std::stoi(std::string({fc, lc}));
+  }
+  fout << sum;
 }
--- First_Day/second_part.cpp
+++ First_Day/second_part.cpp
@@ -1,34 +1,41 @@
 #include <fstream>
+#include <iostream>
 #include <string>
 int main() {
-	long long sum = 0;
-	std::string[] numbers = {"zero","one","two","three","four","five","six","seven","eight","nine"}
-	std::ifstream fin = std::ifstream("input.txt");
-	std::ofstream fout = std::ofstream("output.txt");
-	std::string line;
-	while (!fin.eof())
-	{
-		short fc = 100;
-		short lc = 100;
-		std::getline(fin, line);
-		for (int i = 0; i < line.length(); i++)
-		{
-			if (std::isdigit(line[i])) {
-				if (fc == 100)
-					fc = line[i];
-				lc = line[i];
-			}
-       else{
-			//parse word if possible
-			for (int i = 0; i < 10; i++) {
-				if(numbers[i] == line.substr(i,numbers[i].length())){
-						fc
-					}
-			}
-			}
-		}
-		sum += std::stoi(std::string({ fc,lc }));
-
-	}
-	fout << sum;
+  long long sum = 0;
+  std::string numbers[] = {"one", "two",   "three", "four", "five",
+                           "six", "seven", "eight", "nine"};
+  std::ifstream fin = std::ifstream("input.txt");
+  std::ofstream fout = std::ofstream("output.txt");
+  std::string line;
+  while (!fin.eof()) {
+    short fc = 100;
+    short lc = 100;
+    std::getline(fin, line);
+    if (line == "")
+      break;
+    for (int i = 0; i < line.length(); i++) {
+      if (std::isdigit(line[i])) {
+        if (fc == 100)
+          fc = line[i] - 0x30;
+        lc = line[i] - 0x30;
+      } else {
+        // parse word if possible
+        for (int j = 0; j < 9; j++) {
+          if (numbers[j] == line.substr(i, numbers[j].length())) {
+            if (fc == 100) {
+              fc = j + 1;
+              // std::cout << "message" << std::endl;
+            }
+            lc = j + 1;
+            // std::cout << "ppkre" << j << std::endl;
+            break;
+          }
+        }
+      }
+    }
+    sum += fc * 10 + lc;
+    std::cout << fc * 10 + lc << '\n';
+  }
+  fout << sum;
 }
